d2de130539fbafdc892a419b0cad8a0f606b4996
Th. Abplanalp <info@tasoft.ch>
2021-02-14 15:17:17 +0100

    FIX: procedure export

--- src/Node/Label.php
+++ src/Node/Label.php
@@ -4,8 +4,30 @@
 namespace Ikarus\SPS\Dev\Node;
 
 
-use Skyline\Render\Specification\Name;
-
-class Label extends Name
+class Label
 {
+	/** @var string */
+	private $name;
+
+	/**
+	 * Name constructor.
+	 * @param string $name
+	 */
+	public function __construct(string $name)
+	{
+		$this->name = $name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	public function __toString()
+	{
+		return $this->getName();
+	}
 }
