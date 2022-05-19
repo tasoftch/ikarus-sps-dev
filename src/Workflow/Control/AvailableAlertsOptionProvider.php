78cd2254e70649d1bf9738dce842b7ed205910e0
Th. Abplanalp <info@tasoft.ch>
2022-05-19 15:28:18 +0200

    ADD: workflows

---
+++ src/Workflow/Control/AvailableAlertsOptionProvider.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, TASoft Applications
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace Ikarus\SPS\Dev\Workflow\Control;
+
+
+use TASoft\Service\ServiceManager;
+use TASoft\Util\PDO;
+
+class AvailableAlertsOptionProvider implements \Skyline\HTML\Form\Control\Option\Provider\OptionProviderInterface
+{
+
+	/**
+	 * @inheritDoc
+	 */
+	public function yieldOptions(?string &$group): \Generator
+	{
+		/** @var PDO $PDO */
+		$PDO = ServiceManager::generalServiceManager()->get("PDO");
+
+		foreach($PDO->select("SELECT
+concat(D.name, '.', ALERT.name) as id,
+       title as label,
+       D.label as groupName
+FROM ikarus_sps.ALERT
+JOIN DOMAIN D ON domain = D.id
+ORDER BY D.label, title") as $record) {
+			$group = $record["groupName"];
+			yield $record['id'] => $record["label"];
+		}
+	}
+}
