0dcbcc38431474de7d5f67f4efdfefb61cab4646
Th. Abplanalp <info@tasoft.ch>
2022-05-31 18:11:53 +0200

    ADD: workflows

---
+++ src/Workflow/Control/RelationEditorControl.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, TASoft Applications
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace Ikarus\SPS\Dev\Workflow\Control;
+
+
+class RelationEditorControl extends \Skyline\HTML\Form\Control\Text\TextAreaControl
+{
+	const RELATION_TYPE_CONDITION = 1;
+	const RELATION_TYPE_CONDITIONS = 2;
+	const RELATION_TYPE_INSTRUCTION = 3;
+	const RELATION_TYPE_INSTRUCTIONS = 4;
+
+	const RELATION_TYPE_SINGLE = 8;
+	const RELATION_TYPE_MULTIPLE = 9;
+
+	const RELATION_TYPE_DEFAULT = self::RELATION_TYPE_MULTIPLE;
+
+	private $relationTypes = self::RELATION_TYPE_DEFAULT;
+
+	/**
+	 * @return int
+	 */
+	public function getRelationTypes(): int
+	{
+		return $this->relationTypes;
+	}
+
+	/**
+	 * @param int $relationTypes
+	 * @return RelationEditorControl
+	 */
+	public function setRelationTypes(int $relationTypes): RelationEditorControl
+	{
+		$this->relationTypes = $relationTypes;
+		return $this;
+	}
+}
